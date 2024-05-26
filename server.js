7a62058f681ca03aaa166ae78c20c0a830cda3ae
yashdhameliya88 <yashdhameliya98@gmail.com>
2024-05-26 12:16:59 +0530

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // Include the 'path' module for file path manipulation
 
 const app = express();
 const PORT = 3001;
@@ -8,6 +9,12 @@ const CSV_DIRECTORY = 'E:/MyData/A/';
 app.use(cors());
 app.use(express.json());
 app.use('/csv-files', express.static(CSV_DIRECTORY));
+
+// Middleware to log when a CSV file is successfully retrieved
+app.use('/csv-files', (req, res, next) => {
+  console.log(`CSV file "${req.path}" was successfully retrieved.`);
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
